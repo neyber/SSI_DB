16b6ab213db88ba2f019675b6ff57f1866e30709
LinetTorrico <linet.torrico@dharbor.com>
2018-05-27 22:09:31 -0400

    WorkItemClassification and WorkItem
    Including audit fields for WorkItemClassification and WorkItem

--- dataBase/Auditoria/SchemaSSI-Audit.sql
+++ dataBase/Auditoria/SchemaSSI-Audit.sql
@@ -38,3 +38,78 @@ ELSE
   PRINT 'Table AuditHistory_SSI already exists into the database';
  END
 GO
+
+
+
+
+
+/******** existing work item *****/
+--Create table WorkItemClassification
+
+IF NOT EXISTS (SELECT *
+   FROM sys.[objects]
+   WHERE Type = 'U'
+   AND object_id = OBJECT_ID('dbo.WorkItemClassification')
+)
+BEGIN
+CREATE TABLE WorkItemClassification (Id INT IDENTITY(1,1) NOT NULL
+					          , name VARCHAR(100) CONSTRAINT NN_Name NOT NULL
+							  , description VARCHAR(200) CONSTRAINT NN_Description NOT NULL
+							  , createdOn DATETIME NOT NULL
+					          , updatedOn DATETIME NOT NULL
+							  , isDeleted BIT
+							  ,	[CreatedBy] [int]  NOT NULL
+							  , [CreatedDate] [datetime]  NOT NULL
+							  , [ModifiedBy] [int]        NOT NULL
+							  , [ModifiedDate] [datetime] NOT NULL
+                    , version BIGINT DEFAULT 1
+                    CONSTRAINT PK_WorkItemClassification PRIMARY KEY(
+                        [Id]
+                    ));
+
+                    ALTER TABLE [dbo].[WorkItemClassification] ADD CONSTRAINT [DF_WorkItemClassification_CreatedOn]  DEFAULT (GETUTCDATE()) FOR [createdOn];
+		            ALTER TABLE [dbo].[WorkItemClassification] ADD CONSTRAINT [DF_WorkItemClassification_UpdatedOn]  DEFAULT (GETUTCDATE()) FOR [updatedOn];
+                    ALTER TABLE [dbo].[WorkItemClassification] ADD CONSTRAINT [DF_WorkItemClassification_IsDeleted]  DEFAULT (0) FOR [isDeleted]
+					ALTER TABLE [dbo].[Department] ADD CONSTRAINT [DF_Department_CreatedDate]  DEFAULT (GETUTCDATE()) FOR [CreatedDate];
+					ALTER TABLE [dbo].[Department] ADD CONSTRAINT [DF_Department_ModifiedDate]  DEFAULT (GETUTCDATE()) FOR [ModifiedDate]
+					
+					PRINT 'Table WorkItemClassification created!';
+END
+ELSE
+	BEGIN
+		PRINT 'Table WorkItemClassification already exists into the database';
+	END
+GO
+
+
+
+--Create table WorkItem
+
+IF NOT EXISTS (SELECT *
+   FROM sys.[objects]
+   WHERE Type = 'U'
+   AND object_id = OBJECT_ID('dbo.WorkItem')
+)
+BEGIN
+CREATE TABLE WorkItem (Id INT IDENTITY(1,1) NOT NULL
+					          , name VARCHAR(100) CONSTRAINT NN_Name NOT NULL
+                    , description VARCHAR(200) CONSTRAINT NN_Description NOT NULL
+                    , workItemClassificationId INT
+                    , createdOn DATETIME NOT NULL
+					          , updatedOn DATETIME NOT NULL
+                    , isDeleted BIT
+                    , version BIGINT DEFAULT 1
+                    CONSTRAINT PK_WorkItem PRIMARY KEY(
+                        [Id]
+                    ));
+
+                    ALTER TABLE [dbo].[WorkItem] ADD CONSTRAINT [DF_WorkItem_CreatedOn]  DEFAULT (GETUTCDATE()) FOR [createdOn];
+		                ALTER TABLE [dbo].[WorkItem] ADD CONSTRAINT [DF_WorkItem_UpdatedOn]  DEFAULT (GETUTCDATE()) FOR [updatedOn];
+                    ALTER TABLE [dbo].[WorkItem] ADD CONSTRAINT [DF_WorkItem_IsDeleted]  DEFAULT (0) FOR [isDeleted]
+					PRINT 'Table WorkItem created!';
+END
+ELSE
+	BEGIN
+		PRINT 'Table WorkItem already exists into the database';
+	END
+GO
