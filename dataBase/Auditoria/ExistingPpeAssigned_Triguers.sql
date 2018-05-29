4be736718035da48773189013de28fab972ea1b2
LinetTorrico <linet.torrico@dharbor.com>
2018-05-29 07:20:21 -0400

    Triguers-LT
    Triguers-LT

---
+++ dataBase/Auditoria/ExistingPpeAssigned_Triguers.sql
@@ -0,0 +1,89 @@
+
+/******************************************************************************
+**  Nombre: TG_ExistingPpeAssigned(Audit)_InsertUpdate
+**  Descripcion: 
+**
+**  Autor: Linet Torrico
+**
+**  Fecha: 05/28/2018
+*******************************************************************************
+**                            Change History
+*******************************************************************************
+**  Fecha:       Autor:           Descripcion:
+** --------     -----------      -----------------------------------------------
+** 05/28/2018   Linet Torrico   Initial version
+*******************************************************************************/
+
+
+
+/*
+** Reviewing the trigger does not exist, if it does, the script will remove it.
+*/
+IF EXISTS (SELECT * FROM sys.objects 
+    WHERE object_id = OBJECT_ID(N'[dbo].[TG_ExistingPpeAssigned(Audit)InsertUpdate]') 
+    AND type = 'TR')
+BEGIN
+  DROP TRIGGER [dbo].[TG_ExistingPpeAssigned(Audit)InsertUpdate];
+  PRINT '[TG_ExistingPpeAssigned(Audit)_InsertUpdate] trigger was removed!';
+END
+GO
+
+
+CREATE TRIGGER [dbo].[TG_ExistingPpeAssigned(Audit)_InsertUpdate]
+
+ON [dbo].[ExistingPpeAssigned]
+
+FOR INSERT, UPDATE
+AS
+BEGIN
+  IF TRIGGER_NESTLEVEL(@@ProcID) > 1 
+    RETURN
+ 
+  SET NOCOUNT ON;
+  SET XACT_ABORT ON;
+ 
+  DECLARE @CurrDate DATETIME = GETUTCDATE();
+ 
+  IF UPDATE(updatedOn)
+  BEGIN
+    INSERT INTO dbo.AuditHistory_SSI(TableName, 
+                                 ColumnName, 
+                                 ID, 
+                                 Date, 
+                                 OldValue, 
+                                 NewValue,
+								 ModifiedBy) 
+    SELECT TableName    = 'ExistingPpeAssigned', 
+           ColumnName   = 'updatedOn',
+           ID1          = i.Id, 
+           Date         = @CurrDate, 
+           OldValue     = d.[updatedOn], 
+           NewValue     = i.[updatedOn],
+           ModifiedBy   = i.ModifiedBy          
+    FROM deleted d 
+    FULL OUTER JOIN inserted i ON (d.Id = i.Id)
+    WHERE ISNULL(d.updatedOn, '') != ISNULL(i.updatedOn, '');
+  END
+
+    IF UPDATE(createdOn)
+  BEGIN
+    INSERT INTO dbo.AuditHistory_SSI(TableName, 
+                                 ColumnName, 
+                                 ID, 
+                                 Date, 
+                                 OldValue, 
+                                 NewValue,
+								 ModifiedBy) 
+    SELECT TableName    = 'ExistingPpeAssigned', 
+           ColumnName   = 'createdOn',
+           ID1          = i.Id, 
+           Date         = @CurrDate, 
+           OldValue     = d.[createdOn], 
+           NewValue     = i.[createdOn],
+           ModifiedBy   = i.ModifiedBy          
+    FROM deleted d 
+    FULL OUTER JOIN inserted i ON (d.Id = i.Id)
+    WHERE ISNULL(d.createdOn, '') != ISNULL(i.createdOn, '');
+  END
+
+END;
---
+++ dataBase/Auditoria/ExistingPpe_Triguers.sql
@@ -0,0 +1,89 @@
+
+/******************************************************************************
+**  Nombre: TG_ExistingPpe(Audit)_InsertUpdate
+**  Descripcion: 
+**
+**  Autor: Linet Torrico
+**
+**  Fecha: 05/28/2018
+*******************************************************************************
+**                            Change History
+*******************************************************************************
+**  Fecha:       Autor:           Descripcion:
+** --------     -----------      -----------------------------------------------
+** 05/28/2018   Linet Torrico   Initial version
+*******************************************************************************/
+
+
+
+/*
+** Reviewing the trigger does not exist, if it does, the script will remove it.
+*/
+IF EXISTS (SELECT * FROM sys.objects 
+    WHERE object_id = OBJECT_ID(N'[dbo].[TG_ExistingPpe(Audit)_InsertUpdate]') 
+    AND type = 'TR')
+BEGIN
+  DROP TRIGGER [dbo].[TG_WorkItem(Audit)_InsertUpdate];
+  PRINT '[TG_ExistingPpe(Audit)_InsertUpdate] trigger was removed!';
+END
+GO
+
+
+CREATE TRIGGER [dbo].[TG_ExistingPpe(Audit)_InsertUpdate]
+
+ON [dbo].[ExistingPpe]
+
+FOR INSERT, UPDATE
+AS
+BEGIN
+  IF TRIGGER_NESTLEVEL(@@ProcID) > 1 
+    RETURN
+ 
+  SET NOCOUNT ON;
+  SET XACT_ABORT ON;
+ 
+  DECLARE @CurrDate DATETIME = GETUTCDATE();
+ 
+  IF UPDATE(updatedOn)
+  BEGIN
+    INSERT INTO dbo.AuditHistory_SSI(TableName, 
+                                 ColumnName, 
+                                 ID, 
+                                 Date, 
+                                 OldValue, 
+                                 NewValue,
+								 ModifiedBy) 
+    SELECT TableName    = 'ExistingPpe', 
+           ColumnName   = 'updatedOn',
+           ID1          = i.Id, 
+           Date         = @CurrDate, 
+           OldValue     = d.[updatedOn], 
+           NewValue     = i.[updatedOn],
+           ModifiedBy   = i.ModifiedBy          
+    FROM deleted d 
+    FULL OUTER JOIN inserted i ON (d.Id = i.Id)
+    WHERE ISNULL(d.updatedOn, '') != ISNULL(i.updatedOn, '');
+  END
+
+    IF UPDATE(createdOn)
+  BEGIN
+    INSERT INTO dbo.AuditHistory_SSI(TableName, 
+                                 ColumnName, 
+                                 ID, 
+                                 Date, 
+                                 OldValue, 
+                                 NewValue,
+								 ModifiedBy) 
+    SELECT TableName    = 'ExistingPpe', 
+           ColumnName   = 'createdOn',
+           ID1          = i.Id, 
+           Date         = @CurrDate, 
+           OldValue     = d.[createdOn], 
+           NewValue     = i.[createdOn],
+           ModifiedBy   = i.ModifiedBy          
+    FROM deleted d 
+    FULL OUTER JOIN inserted i ON (d.Id = i.Id)
+    WHERE ISNULL(d.createdOn, '') != ISNULL(i.createdOn, '');
+  END
+
+END;
